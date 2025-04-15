e20cb36db9f4dbca6934e132bfa886297a7f4c37
Ray <rkwan@skatescribe.com>
2025-04-15 14:09:06 -0400

    refactor types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,42 @@
 "use client";
 
+import { ColoredLetter, Letter } from "@/ui/letter";
 import {
   get_five_letter_words,
   is_valid_five_letter_word,
 } from "@/lib/api/five_letter_words";
 import { useEffect, useState } from "react";
 
-import { Letter } from "@/ui/letter";
 import { get_random_word } from "@/lib/word_generator";
+
+interface Guess {
+  word_list: Letter[];
+  word: string;
+}
+
+const COLORS = {
+  GREEN: "bg-green-500",
+  YELLOW: "bg-yellow-400",
+  GREY: "bg-gray-400",
+};
 
 export default function Home() {
   const [five_letter_words, set_five_letter_words] = useState<string[]>([]);
   const [word, set_word] = useState<string | null>(null);
-  const [guesses, set_guesses] = useState<string[]>([]);
+  const [guesses, set_guesses] = useState<Guess[]>([]);
   const [input, set_input] = useState("");
 
   const handle_guess = async () => {
     if (input.length !== 5) return;
-    else if (!(await is_valid_five_letter_word(input))) {
-      alert("Word is not in the system");
-      return;
-    } else if (guesses.includes(input)) {
+    // else if (!(await is_valid_five_letter_word(input))) {
+    //   alert("Word is not in the system");
+    //   return;
+    // }
+    else if (guesses.filter((guess) => guess.word === input).length > 0) {
       alert("Word already guessed");
       return;
     }
-    set_guesses([...guesses, input.toUpperCase()]);
+    set_guesses([...guesses, format_guess(input)]);
     set_input("");
   };
 
@@ -35,42 +47,47 @@ export default function Home() {
    * - Returns yellow if the letter exists in the word but in the wrong position.
    * - Returns gray if the letter does not exist in the word.
    *
-   * @param {string} guess - The guessed word (must be the same length as the actual word).
-   * @param {number} index - The index of the current letter being evaluated.
-   * @returns {string} A Tailwind CSS background color class representing the letter's correctness.
+   * @param {string} input - The guessed word (must be the same length as the actual word).
+   * @returns {Guess} A Tailwind CSS background color class representing the letter's correctness.
    */
-  const get_letter_color = (guess: string, index: number): string => {
-    if (word === null) return "";
+  const format_guess = (input: string): Guess => {
+    if (word === null) return { word_list: [], word: "" };
 
     const used_idxs: number[] = [];
-    const colors: ("bg-green-500" | "bg-gray-400" | "bg-yellow-400")[] = word
-      .split("")
-      .map((letter, index) => {
-        if (letter === guess[index]) {
-          used_idxs.push(index);
-          return "bg-green-500";
-        }
-        return "bg-gray-400";
-      });
+    const colored_letters: Letter[] = input.split("").map((letter, index) => {
+      if (letter === word[index]) {
+        used_idxs.push(index);
+        return {
+          value: letter,
+          color: COLORS.GREEN,
+        };
+      }
+      return {
+        value: letter,
+        color: COLORS.GREY,
+      };
+    });
 
     // Second pass: Check for present but misplaced letters (yellow)
-    guess.split("").forEach((char, i) => {
-      if (colors[i] === "bg-green-500") return;
+    input.split("").forEach((char, i) => {
+      if (colored_letters[i].color === COLORS.GREEN) return;
 
       word.split("").forEach((w_char, j) => {
         if (
           !used_idxs.includes(j) &&
           char === w_char &&
-          colors[i] !== "bg-yellow-400"
+          colored_letters[i].color !== COLORS.YELLOW
         ) {
-          colors[i] = "bg-yellow-400";
+          colored_letters[i].color = COLORS.YELLOW;
           used_idxs.push(j);
         }
       });
     });
-    //
-    const color = colors[index];
-    return color;
+
+    return {
+      word_list: colored_letters,
+      word: input,
+    };
   };
 
   useEffect(() => {
@@ -91,12 +108,8 @@ export default function Home() {
       <div className="space-y-2 mb-4">
         {guesses.map((guess, i) => (
           <div key={i} className="flex space-x-2">
-            {guess.split("").map((letter, j) => (
-              <Letter
-                key={j}
-                guess={letter}
-                letter_color={get_letter_color(guess, j)}
-              />
+            {guess.word_list.map((letter, j) => (
+              <ColoredLetter key={j} guess={letter} />
             ))}
           </div>
         ))}
@@ -117,10 +130,7 @@ export default function Home() {
             className="dark:text-white text-black px-3 py-2 rounded mb-2 w-40 text-center"
             placeholder="Guess a word"
           />
-          <button
-            onClick={handle_guess}
-            className="bg-blue-500 px-4 py-2 rounded hover:bg-blue-600"
-          >
+          <button className="bg-blue-500 px-4 py-2 rounded hover:bg-blue-600">
             Submit
           </button>
         </form>
--- src/ui/letter.tsx
+++ src/ui/letter.tsx
@@ -1,14 +1,19 @@
-interface LetterProps {
-  guess: string;
-  letter_color: string;
+export interface Letter {
+  value: string;
+  color: string;
 }
-export const Letter = ({ guess, letter_color }: LetterProps) => {
+
+interface LetterProps {
+  guess: Letter;
+}
+
+export const ColoredLetter = ({ guess }: LetterProps) => {
   return (
     <div className="flex space-x-2">
       <div
-        className={`w-12 h-12 flex items-center justify-center text-xl font-bold rounded ${letter_color}`}
+        className={`w-12 h-12 flex items-center justify-center text-xl font-bold rounded ${guess.color}`}
       >
-        {guess}
+        {guess.value}
       </div>
     </div>
   );
